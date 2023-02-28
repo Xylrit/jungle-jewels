480949db72aceabbde28209fee268c912c4cfcfa
Xylrit <111688120+Xylrit@users.noreply.github.com>
2023-02-28 15:37:09 +0800

    performance-update1.0
    compressed images
    renamed files
    added alt tags
    styled the contact form a bit

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-const menu = document.querySelector("#mobile-menu");
-const menuLinks = document.querySelector('.nav-menu');
-
-menu.addEventListener("click", function(){
-	menu.classList.toggle('is-active');
-	menuLinks.classList.toggle('active');
-})
-
-const nav = document.querySelector(".nav-container");
-let lastScrollY = window.scrollY;
-
-window.addEventListener("scroll", () => {
-	if (lastScrollY < window.scrollY) {
-		nav.classList.add("nav--hidden");
-	}
-	else{
-		nav.classList.remove("nav--hidden");
-	}
-
-	lastScrollY = window.scrollY;
-});
-
-const dropbtn = document.querySelector('.dropbtn')
-const dropdownContent = document.querySelector('.dropdown-content')
-const dropdownContainer = document.querySelector('.dropdown-container')
-
-dropbtn.addEventListener("click", function(){
-	dropdownContent.classList.toggle('is-active');
-	dropdownContainer.classList.toggle('is-active');
-})
-
-document.getElementById("inquireBtn").onclick = function () {
-	location.href = "contact.html";
-}
 
 let allImages = document.querySelectorAll("img");
 allImages.forEach((value)=>{
